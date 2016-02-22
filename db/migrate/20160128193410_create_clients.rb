454b61d8ff4a81642d0419aaa630fbc511814260
Lisa Westlund <lw222gu@student.lnu.se>
2016-02-22 01:06:46 +0100

    Fixed lint errors: Now using hash notation

--- db/migrate/20160128193410_create_clients.rb
+++ db/migrate/20160128193410_create_clients.rb
@@ -1,11 +1,11 @@
 class CreateClients < ActiveRecord::Migration
   def change
     create_table :clients do |t|
-      t.string :name, :null => false, :limit => 50
-      t.string :description, :limit => 250
+      t.string :name, null: false, limit: 50
+      t.string :description, limit: 250
       t.string :url
       t.string :key
-      t.boolean :active, :null => false, :default => true
+      t.boolean :active, null: false, default: true
       t.belongs_to :user, index: true
       t.timestamps null: false
     end
