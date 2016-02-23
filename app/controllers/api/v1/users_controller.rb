7e255e8aaa1451bbeabb2bbf1d03625a2f589500
Lisa Westlund <lw222gu@student.lnu.se>
2016-02-23 23:42:25 +0100

    Fixed inheritance error and commented some code

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::UsersController < ApplicationController
+class Api::V1::UsersController < Api::V1::BaseController
   def show
     u = User.find(params[:id])
     render(json: Api::v1::UserSerializer.new(u).to_json)
