748acb8808e7c4c264fc3a97ceff1b632cbcffa0
Lisa Westlund <lw222gu@student.lnu.se>
2016-02-24 12:16:18 +0100

    Create works for Tags

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -14,4 +14,31 @@ class Api::V1::TagsController < Api::V1::BaseController
     tag = Tag.find(params[:id])
     render(json: Api::V1::TagSerializer.new(tag).to_json)
   end
+
+  def create
+    tag = Tag.new(create_params)
+    # TODO: add functionality for returning an error if salary is not valid.
+    tag.save! # TODO: If not valid, ActiveRecord::recordInvalid. Add rescue.
+    render(
+      json: tag,
+      status: 201,
+      location: api_v1_tag_path(tag.id),
+      serializer: Api::V1::TagSerializer
+    )
+  end
+
+  def destroy
+  end
+
+  private
+
+  def create_params
+    parameters = ActionController::Parameters.new(
+      tag:
+      {
+        tag: params[:tag]
+      }
+    )
+    parameters.require(:tag).permit(:tag)
+  end
 end
