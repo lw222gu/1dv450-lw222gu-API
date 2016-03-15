6021ee852bcdb7417b3d480356729eae8501e21f
Lisa Westlund <lw222gu@student.lnu.se>
2016-03-15 20:14:53 +0100

    Added user_id in authentication response.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::SessionsController < Api::V1::BaseController
   def auth
     resource_owner = ResourceOwner.find_by(username: params[:username])
     if resource_owner && resource_owner.authenticate(params[:password])
-      render json: { token: encode_JWT(resource_owner) }
+      render json: { token: encode_JWT(resource_owner), user_id: resource_owner.id }
     else
       render json: { status: 401, error: 'Invalid user credentials.' }, status: :unauthorized
     end
